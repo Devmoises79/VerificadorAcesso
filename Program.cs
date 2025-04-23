9a3e797d00b4332d9268b8fe9a731159a32fa522
Devmoises79 <devmoisesss@gmail.com>
2025-04-23 13:08:13 -0300

    VerificaAcesso

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program 
+{
+    static void Main()
+    {
+
+    Console.Write("Digite seu nome:  "); //pede o nome
+    string nome = Console.ReadLine(); // recebe o nome
+
+    Console.Write("Digite sua idade:  "); // pede a idade
+    int idade = int.Parse(Console.ReadLine()); // recebe a idade
+
+    bool nomeNaLista = nome == "Joao" || nome == "Maria" || nome == "Pedro"; //verifica o acesso
+
+    if (nomeNaLista && idade >= 18) // verifica se pode entrar
+    {
+        Console.WriteLine("Pode entrar!"); // acesso permitido
+    }
+    else
+    {
+        Console.WriteLine("Não pode entrar!");// acesso negado
+
+    }
+
+    }
+}
+
+
